dd48098a1c328f08e40d1157c5a92afd828eac37
SeanM322 <sean@rair.tech>
2024-11-05 18:51:46 +0200

    change

--- dev-dapp/src/components/common/rairSDK.ts
+++ dev-dapp/src/components/common/rairSDK.ts
@@ -1,7 +1,7 @@
 import { RairSDK } from "@rair-protocol/sdk";
 
 const settings = {
-  serverURL: "http://localhost:3001",
+  serverURL: import.meta.env.VITE_SDK_MAIN_URL,
 };
 
 export const rairSDK = new RairSDK(settings);
