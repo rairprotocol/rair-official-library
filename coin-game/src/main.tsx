7f9047d5262a742e38b920719c0c9b4dd66720cd
Juan Miguel Sanchez Mola <jmsm412@gmail.com>
2025-07-11 08:32:01 -0400

    Coin game JWT

--- coin-game/src/main.tsx
+++ coin-game/src/main.tsx
@@ -3,9 +3,24 @@ import React, { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider } from 'react-redux';
+import Axios from 'axios';
 
 import { GlobalModalStateProvider } from './providers/ModalProvider/ModalProvider';
 import { store } from './redux/store';
+
+
+Axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('rair-jwt');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 const sentryIoTraceRate = Number(import.meta.env.VITE_SENTRY_IO_TRACE_RATE);
 import {
